0fffd9761195737320905278fdbb30f80f86f8f8
lemoras <oyasarid@gmail.com>
2020-05-10 02:47:46 +0300

    Revert "path configured"
    This reverts commit 27e6e1a6c0f26d629ed3e5db8076471aed01cfec.

--- system/app.js
+++ system/app.js
@@ -138,17 +138,17 @@ var authPath = "/user";
     function config($routeProvider, $locationProvider, $ocLazyLoadProvider, $qProvider) {
         $qProvider.errorOnUnhandledRejections(false);
         $routeProvider.when('/login', {
-            templateUrl: 'http://lemoras.site/system/modules/login/login.view.html',
+            templateUrl: '../../system/modules/login/login.view.html',
             controller: 'LoginController',
             controllerAs: 'vm'
         })
             .when('/logout', {
-                templateUrl: 'http://lemoras.site/system/modules/logout/logout.view.html',
+                templateUrl: '../../system/modules/logout/logout.view.html',
                 controller: 'LogoutController',
                 controllerAs: 'vm'
             })
             .when('/register', {
-                templateUrl: 'http://lemoras.site/system/modules/register/register.view.html',
+                templateUrl: '../../system/modules/register/register.view.html',
                 controller: 'RegisterController',
                 controllerAs: 'vm'
             });  // .when('/load', ( window.location.href = './index.html' ) );
